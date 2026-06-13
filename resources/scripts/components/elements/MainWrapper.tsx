b5054de05bf0a84975a538f5922edab3a62bacfd
intelstriker <intelstriker21@gmail.com>
2026-06-13 11:36:32 +0100

    Update MainWrapper.tsx

--- resources/scripts/components/elements/MainWrapper.tsx
+++ resources/scripts/components/elements/MainWrapper.tsx
@@ -4,7 +4,7 @@ const MainWrapper = styled.div`
     width: 100%;
     height: 100%;
     border-radius: 0.375rem;
-    background: radial-gradient(124.75% 124.75% at 50.01% -10.55%, rgb(16, 16, 16) 0%, rgb(4, 4, 4) 100%);
+    background: radial-gradient(124.75% 124.75% at 50.01% -10.55%, #1a0a2e 0%, #05010a 100%);
 `;
 
 export default MainWrapper;
